49064e94f33b4319bce7eac833b468f1b9d07171
Ildus <qwzxaser2015@gmail.com>
2025-11-30 20:58:04 +0300

    task_2 done

---
+++ semester_2/test_2/task_2.cpp
@@ -0,0 +1,50 @@
+#include <fstream>
+#include <cmath>
+#include <iomanip>
+
+double phi_1(double x, double y)
+{
+    return std::sin(y)/2 - 0.8 + 0.0 * x;
+}
+
+double phi_2(double x, double y)
+{
+    return 0.8 - std::cos(x + 0.5) + 0.0 * y;
+}
+
+struct Vector
+{
+    double x_;
+    double y_;
+};
+
+double norm(Vector x, Vector y)
+{
+    return std::sqrt((x.x_ - y.x_)*(x.x_ - y.x_) + (x.y_ - y.y_)*(x.y_ - y.y_));
+}
+
+int main()
+{
+    double x = 0.0;
+    double y = 0.0;
+    double eps = 0.001;
+    double new_x = 0.0;
+    double new_y = 0.0;
+    std::ofstream file("task_2.txt");
+    file << "x_0 = " << x << " y_0 = " << y << std::endl << std::endl;
+    if (!file.is_open())
+        return 1;
+    for(int i = 1; i < 1000; i++)
+    {
+        new_x = phi_1(x, y);
+        new_y = phi_2(x, y);
+        file << std::scientific << std::setprecision(7) << "x_" << i << " = " <<  new_x << " y_" << i << " = "  << new_y << std::endl;
+        file << "||x_" << i << " - x_" << i-1 << "|| = " << norm({new_x, new_y}, {x, y}) << std::endl << std::endl;
+        if (norm({new_x, new_y}, {x, y}) < eps)
+            break;
+        x = new_x;
+        y = new_y;    
+    }
+    file.close();
+    return 0;
+}
